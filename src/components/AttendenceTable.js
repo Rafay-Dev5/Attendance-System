6c17ecafc350f4f824c823e22d894dc40b905e34
Rafay-Dev5 <rafay7381@gmail.com>
2023-01-11 17:04:54 +0400

    table completed

--- src/components/AttendenceTable.js
+++ src/components/AttendenceTable.js
@@ -9,8 +9,9 @@ import Select from "@mui/material/Select";
 
 import moment from "moment";
 
+//const uri = "https://localhost:44356";
 const columns = [
-  { field: "std_ID", headerName: "ID", width: 90 },
+  //{ field: "id", headerName: "ID", width: 90 },
   {
     field: "std_name",
     headerName: "Name",
@@ -29,21 +30,10 @@ const columns = [
   },
 ];
 
-const teachers = [
-  { teach_ID: 1, teach_name: "Saif" },
-  { teach_ID: 2, teach_name: "Bilal" },
-  { teach_ID: 3, teach_name: "Abdul Rafay" },
-];
-
-const courses = [
-  { course_ID: 1, course_name: "IPT" },
-  { course_ID: 2, course_name: "Information Security" },
-  { course_ID: 3, course_name: "PPIT" },
-  { course_ID: 4, course_name: "OS" },
-];
-
 const AttendanceTable = () => {
-  const [sendData, setSendData] = useState({});
+  const [loading, setLoading] = useState(true);
+
+  const [sendData, setSendData] = useState();
   const [teacher, setTeacher] = useState(1);
   const [teacherData, setTeacherData] = useState([]); //teacher dropdown list
   const [course, setCourse] = useState(1);
@@ -51,25 +41,25 @@ const AttendanceTable = () => {
   const [tableData, setTableData] = useState([
     //Data for datagrid table
     {
-      std_ID: 1,
+      id: 1,
       std_name: "Snow",
       date: moment().format("DD-MM-YYYY"),
       attendance1: "P",
     },
     {
-      std_ID: 2,
+      id: 2,
       std_name: "Lannister",
       date: moment().format("DD-MM-YYYY"),
       attendance1: "P",
     },
     {
-      std_ID: 3,
+      id: 3,
       std_name: "Lanny",
       date: moment().format("DD-MM-YYYY"),
       attendance1: "A",
     },
     {
-      std_ID: 4,
+      id: 4,
       std_name: "Stark",
       date: moment().format("DD-MM-YYYY"),
       attendance1: "P",
@@ -77,41 +67,52 @@ const AttendanceTable = () => {
   ]);
 
   useEffect(() => {
-    //Abdul Rafay uncomment this while integration
-    // fetch("/api/Std")
-    //   .then((res) => {
-    //     setTableData(
-    //       res.map((e) => {
-    //         return {
-    //           std_ID: e.std_ID,
-    //           std_name: e.std_name,
-    //           teach_ID: 1,
-    //           course_ID: 1,
-    //           date: moment().format("DD-MM-YYYY"),
-    //           attendance1: "",
-    //         };
-    //       })
-    //     );
-    //   })
-    //   .catch((error) => console.log(error));
-    // fetch("/api/Teach")
-    //   .then((res) => {
-    //     setTeacherData(res);
-    //   })
-    //   .catch((error) => console.log(error));
-    // fetch("/api/Cor")
-    //   .then((res) => {
-    //     setCourseData(res);
-    //   })
-    //   .catch((error) => console.log(error));
+    fetch(`/api/Std`)
+      .then((res) =>
+        res.json().then((json) => {
+          console.log(json);
+          json = json?.map((e) => Object.assign(e, { id: e.std_ID }));
+          setTableData(
+            json.map((e) => {
+              return {
+                id: e.id,
+                std_name: e.std_name,
+                teach_ID: 1,
+                course_ID: 1,
+                date: moment().format("DD-MM-YYYY"),
+                attendance1: "A",
+              };
+            })
+          );
+        })
+      )
+      .catch((error) => console.log(error));
+
+    fetch(`/api/Teach`)
+      .then((res) =>
+        res.json().then((json) => {
+          console.log(json);
+          setTeacherData(json);
+        })
+      )
+      .catch((error) => console.log(error));
+    fetch(`/api/Cor`)
+      .then((res) =>
+        res.json().then((json) => {
+          setCourseData(json);
+        })
+      )
+      .catch((error) => console.log(error));
+
+    setLoading(false);
   }, []);
 
   const processRowUpdate = (newRow) => {
+    const updatedRow = { ...newRow, isNew: false };
     setTableData(
-      tableData.map((row) =>
-        row.std_ID === newRow.std_ID && row.date === newRow.date ? newRow : row
-      )
+      tableData.map((row) => (row.id === newRow.id ? updatedRow : row))
     );
+    return updatedRow;
   };
 
   const handleTeacherChange = (event) => {
@@ -122,36 +123,43 @@ const AttendanceTable = () => {
     setCourse(event.target.value);
   };
 
-  const handleSubmit = async () => {
-    setSendData(
-      tableData.map((e) => {
-        return {
-          std_ID: e.std_ID,
-          teach_ID: teacher,
-          course_ID: course,
-          attendance1: e.attendance1,
-          date: e.date,
-        };
-      })
-    );
+  const handleSubmit = () => {
+    let temp = [];
+    tableData.forEach((e) => {
+      temp.push({
+        std_ID: e.id,
+        teach_ID: teacher,
+        course_ID: course,
+        attendance1: e.attendance1,
+        date: e.date,
+      }); //)
+    });
 
-    putData();
+    // const temp1 = temp.map((e) => {
+    //   delete e["id"];
+    //   delete e["isNew"];
+    //   return e;
+    // });
+
+    console.log(temp);
+    putData(temp);
   };
 
-  const putData = () => {
+  const putData = (temp) => {
+    console.log(temp);
     const requestOptions = {
-      method: "PUT",
+      method: "POST",
       headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(sendData),
+      body: JSON.stringify(temp),
     };
-    fetch(`/api/Att/${1}`, requestOptions).then((response) =>
+    fetch(`/api/Att`, requestOptions).then((response) =>
       console.log(response.json())
     );
   };
 
   return (
     <Grid container alignItems="center" justify="center" sx={{ my: 3 }}>
-      <Grid item xs={12} md={6} sx={{ my: 2 }}>
+      <Grid key="teacher" item xs={12} md={6} sx={{ my: 2 }}>
         <FormControl fullWidth>
           <InputLabel id="teacher-label">Teacher</InputLabel>
           <Select
@@ -161,17 +169,15 @@ const AttendanceTable = () => {
             label="Teacher"
             onChange={handleTeacherChange}
           >
-            {
-              /*teacherData.*/ teachers.map((e) => (
-                <MenuItem key={e} value={e.teach_ID}>
-                  {e.teach_name}
-                </MenuItem>
-              ))
-            }
+            {teacherData.map((e) => (
+              <MenuItem key={e.teach_ID} value={e.teach_ID}>
+                {e.teach_name}
+              </MenuItem>
+            ))}
           </Select>
         </FormControl>
       </Grid>
-      <Grid item xs={12} md={6} sx={{ my: 2 }}>
+      <Grid key="course" item xs={12} md={6} sx={{ my: 2 }}>
         <FormControl fullWidth>
           <InputLabel id="course-label">Course</InputLabel>
           <Select
@@ -182,8 +188,8 @@ const AttendanceTable = () => {
             onChange={handleCourseChange}
           >
             {
-              /*courseData.*/ courses.map((e) => (
-                <MenuItem key={e} value={e.course_ID}>
+              /*courseData.*/ courseData.map((e) => (
+                <MenuItem key={e.course_ID} value={e.course_ID}>
                   {e.course_name}
                 </MenuItem>
               ))
@@ -194,8 +200,9 @@ const AttendanceTable = () => {
 
       <Box sx={{ height: 400, width: "100%" }}>
         <DataGrid
-          getRowId={(row) => row.std_ID}
+          //getRowId={(row) => row.id}
           rows={tableData}
+          editMode="row"
           columns={columns}
           pageSize={5}
           rowsPerPageOptions={[5]}
@@ -203,13 +210,14 @@ const AttendanceTable = () => {
           experimentalFeatures={{ newEditingApi: true }}
           processRowUpdate={processRowUpdate}
           onProcessRowUpdateError={(error) => console.log(error)}
+          loading={loading}
         />
       </Box>
 
       <Button
         variant="contained"
         color="secondary"
-        onClick={handleSubmit}
+        onClick={() => handleSubmit()}
         sx={{ width: "50%", mx: "25%" }}
       >
         Update Sheet
